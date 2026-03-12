9e94fca3d8413fd03b70edd802aae1525dac205c
ferriyusra <ferisakuragi@gmail.com>
2026-03-12 17:44:43 +0700

    change wording

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -269,7 +269,7 @@ export default function Hero() {
 						marginBottom: 16,
 						letterSpacing: '-0.02em',
 					}}>
-					Currently Focused Full Stack Engineer Because AI Is Overpower AI :D
+					Currently Focused Full Stack Engineer Because AI Overpower :D
 				</motion.p>
 
 				{/* Tagline */}
